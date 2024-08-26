e0728decc17d97ce008a8368e207eb7101938fbd
Joas Bezerra <joasbz@gmail.com>
2024-08-26 10:52:04 -0400

    home screen

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,23 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val edtHeight = findViewById<TextInputEditText>(R.id.edt_height)
+        val edtWeight = findViewById<TextInputEditText>(R.id.edt_weight)
+        val calcButton = findViewById<Button>(R.id.button)
+
+        calcButton.setOnClickListener {
+            val weight = edtWeight.text
+            val height = edtHeight.text
+            val result =
+        }
     }
 }
